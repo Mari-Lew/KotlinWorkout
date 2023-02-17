a7ee635fc5c533c97440fba5c4ad05306801d434
Marissa Lewinski <101777634+Mari-Lew@users.noreply.github.com>
2023-02-17 11:44:58 -0600

    140. UI setup

--- app/src/main/java/com/example/kotlinworkoutapp/MainActivity.kt
+++ app/src/main/java/com/example/kotlinworkoutapp/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.kotlinworkoutapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    var flStart : FrameLayout? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        flStart = findViewById(R.id.flStart)
+        flStart?.setOnClickListener{
+            Toast.makeText(
+                this@MainActivity,
+                "Click",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
     }
 }
